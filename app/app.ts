b4d617f8686cdaad99fcfbcabb2a3b7978facbd8
Enzo Gebauer <enzo.gebauer@aln.senaicimatec.edu.br>
2023-02-27 21:57:44 -0300

    opa

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,11 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-const crontroller = new NegociacaoController
+const crontroller = new NegociacaoController();
 
 const form = document.querySelector(".form")
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   crontroller.adiciona();
+  crontroller.limparFormulario();
 })
